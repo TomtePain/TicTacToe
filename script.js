53cf89e36e67ef8f78b281b0f5fbeb0a6851ce70
TomtePain <scrennsaver@web.de>
2023-01-30 12:30:37 +0100

    bugfix

--- script.js
+++ script.js
@@ -4,24 +4,30 @@ let currentShape = 'cross';
 
 
 function fillShape(id) {
-    if(currentShape == 'cross') {
-        currentShape = 'circle';
-    } else {
-        currentShape = 'cross';
+    if (!fields[id]) {
+        if (currentShape == 'cross') {
+            currentShape = 'circle';
+        } else {
+            currentShape = 'cross';
+        }
+        fields[id] = currentShape;
+        console.log(fields)
+        draw();
+        checkForWin();
     }
-    fields[id] = currentShape;
-    console.log(fields)
-    draw();
-    checkForWin();
 }
 
 function draw() {
-    for(let i = 0; i < fields.length; i++) {
-        if(fields[i] == 'circle') {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
-        } 
-        if(fields[i] == 'cross') {
+            document.getElementById('player1').classList.add('player-inactive')
+            document.getElementById('player2').classList.remove('player-inactive')
+        }
+        if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
+            document.getElementById('player1').classList.remove('player-inactive')
+            document.getElementById('player2').classList.add('player-inactive')
         }
     }
 }
@@ -29,34 +35,34 @@ function draw() {
 function checkForWin() {
     let winner;
     // Horizontal
-    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
     }
-    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
     }
-    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
     }
     // Senkrecht
-    if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
     }
-    if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
     }
-    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
     }
     // Vertikal
-    if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
     }
-    if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
     }
-    if(winner){
+    if (winner) {
         console.log('GEWONNEN', winner)
     }
-    
+
 }
